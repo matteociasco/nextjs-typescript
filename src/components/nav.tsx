682b2e3622ccb6d355361d00854ea83808975e10
matteociasco <matteo.ciasco@gmail.com>
2020-08-29 10:11:01 +0200

    Boilerplate NextJs app with Typescript support

---
+++ src/components/nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <ul>
+      </ul>
+    </ul>
+  </nav>
+);
+
+export default Nav;
